fdd4afbacacc52753f00aa0f41cb669a30cc6f13
anooj ayyappan <a.anooj@gmail.com>
2020-04-18 17:02:36 -0400

    added simple contentful product

--- services/contentful.js
+++ services/contentful.js
@@ -1,13 +1,11 @@
 const contentful = require("contentful");
 const client = contentful.createClient({
-  // This is the space ID. A space is like a project folder in Contentful terms
   space: "kfgj374ccsy2",
-  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
   accessToken:
-    "d165454ed2486dd20fe741fa7b01fcd575f5bd0c11d78fbc065603c3cf11e941"
+    "7f3b9d4d5587ac6057284dba5706ecefc294f82da91f8f9cc46d17121210ff71",
+  host: "preview.contentful.com"
 });
-// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
 
-export const getEntry = entryID => {
-  return client.getEntry(entryID);
+export const getEntry = entryId => {
+  return client.getEntry(entryId);
 };
--- src/App.js
+++ src/App.js
@@ -4,21 +4,32 @@ import { getEntry } from "../services/contentful";
 import "./styles.css";
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      message: "hello"
+      fields: {
+        productName: "",
+        image: []
+      }
     };
-    getEntry("7LAnCobuuWYSqks6wAwY2a")
+  }
+
+  componentDidMount() {
+    getEntry("4BqrajvA8E6qwgkieoqmqO")
       .then(entry => this.setState(entry))
       .catch(err => this.setState(err));
   }
 
   render() {
+    const {
+      fields: { productName, productDescription, image }
+    } = this.state;
+    const img = image.map(img => <img src={img.fields.file.url} alt="image" />);
     return (
       <div className="App">
-        <h1>Hello {this.state.message}</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <h1>{productName}</h1>
+        <div className="image__container">{img}</div>
+        <h2>{productDescription}</h2>
       </div>
     );
   }
